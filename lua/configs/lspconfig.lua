ce1be274f8370093f68ac66fa2ad915131d023f8
Davey Boyle <davey@elucidan.com>
2024-04-30 23:36:16 +0100

    customisations part 1

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -5,6 +5,8 @@ local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
 local servers = { "html", "cssls" }
+local util = require "lspconfig/util"
+
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
@@ -21,3 +23,34 @@ lspconfig.tsserver.setup {
   on_init = on_init,
   capabilities = capabilities,
 }
+
+lspconfig.gopls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = { "gopls" }, 
+  filetypes={"go", "gomod", "gowork", "gotmpl"},
+  root_dir = util.root_pattern("go.work", "go.mod", ".git")
+}
+ 
+
+lspconfig.dartls.setup{
+   cmd = { 'dart', 'language-server', '--protocol=lsp' },
+    filetypes = { 'dart' },
+    root_dir = util.root_pattern 'pubspec.yaml',
+    init_options = {
+      onlyAnalyzeProjectsWithOpenFiles = true,
+      suggestFromUnimportedLibraries = true,
+      closingLabels = true,
+      outline = true,
+      flutterOutline = true,
+    },
+    settings = {
+      dart = {
+        completeFunctionCalls = true,
+        showTodos = true,
+      },
+  },
+
+}
+
+ 
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,9 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "K",vim.lsp.buf.hover)
 
+map("n", "gd", vim.lsp.buf.definition)
+
+map("n", "ca", vim.lsp.buf.code_action)
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
